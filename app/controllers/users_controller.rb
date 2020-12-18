6823b9de4c486398cc82580ce2e0d3623263ed4a
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-18 12:07:15 -0400

    User controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+    def index
+        puts "mmg"
+    end
+
+end
--- db/migrate/20201218150844_create_tasks.rb
+++ db/migrate/20201218150844_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, index: true
       t.string :name
       t.integer :amount
 
