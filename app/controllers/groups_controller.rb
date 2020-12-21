315c6202e1f1e73ae4283e504f858811b090e2b1
Elbert Corniell <elberthcorniell@gmail.com>
2020-12-21 11:41:20 -0400

    Group creation added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,11 +2,21 @@ class GroupsController < ApplicationController
     before_action :user_logged_in, only: [:show]
 
     def create
-        
-    end
-
-    def show
+        current_user.groups.create!(group_params)
         @groups = current_user.groups
         @new_group = current_user.groups.build
+        redirect_to app_path
+    end
+    
+    def show
+        @user = current_user
+        @groups = current_user.groups
+        @new_group = current_user.groups.build
+    end
+
+    private
+    
+    def group_params
+        params.require(:group).permit(:name, :icon)
     end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,13 +2,18 @@ module GroupsHelper
     def render_groups
         out = ""
         @groups.each do |group|
-            out << "<div class=\"card w-100 h-100\">
-                        <img src=\"#{group.icon}\" onerror=\"if (this.src != 'error.jpg') this.src = 'error.jpg';\" class=\"card-img-top\" alt=\"...\">
-                        <div class=\"card-title\">
-                            <h2>#{group.title}</h2>
+            out << "<div class=\"col-3\">
+                        <div class=\"card w-100 h-100\">
+                            <div class=\"image-setter\">
+                                <img src=\"#{group.icon}\" class=\"card-img-top\" alt=\"...\"  onerror=\"this.onerror=null;this.src='/assets/error.jpg';\">
+                            </div>
+                            <div class=\"card-body\">
+                                <h2><strong>#{group.name}</strong></h2>
+                            </div>
                         </div>
                     </div>"
         end
         out.html_safe
     end
+
 end
